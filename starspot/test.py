682f8767653ff13c54e917b8f5338d774b9b330f
Steven <christophe.steven@gmail.com>
2015-06-01 14:22:22 +0200

    can now test a range of values for each free parameter

--- starspot/test.py
+++ starspot/test.py
@@ -23,29 +23,36 @@ for i in np.arange(len(Teff)):
 #Save magnitudes in a new file with previous quantities
 isochrone.save_unspotted(isodata,magnitudes)
 
+#values of free parameters
 
-#Compute the effect of spots on stars
+zeta=np.arange(1,2)	#luminosity ratio
+epsilon=np.arange(1,2)	#surface ratio
+rho=np.arange(0.001,1)	#spot coverage
+pi=np.arange(1,2) #Tspot/Tphot
 
-zeta=1	#luminosity ratio
-epsilon=1	#surface ratio
-rho=0.001	#spot coverage
-pi=	1	#Tspot/Tphot
+for m in np.arange(len(zeta)) :
+	for n in np.arange(len(epsilon)) :
+		for p in np.arange(len(rho)) :
+			for q in np.arange(len(pi)) :
+				#Compute the effect of spots on stars
+				spots_params = [zeta[m],epsilon[n],rho[p],pi[q]]
+				isodata_spots, nlog_g, Tphot, log_Lphot, Tspot, log_Lspot = \
+						isochrone.add_spots(isodata,spots_params)
 
-spots_params = [zeta,epsilon,rho,pi]
-isodata_spots, nlog_g, Tphot, log_Lphot, Tspot, log_Lspot = \
-		isochrone.add_spots(isodata,spots_params)
+				#Compute magnitudes of spotted stars
+				magnitudes_spots=[]
+				for i in np.arange(len(Tspot)):
+					if i == 0:
+						magnitudes_spots =isochrone.colorize(Tspot[i],
+								nlog_g[i],log_Lspot[i]) + \
+								isochrone.colorize(Tphot[i],nlog_g[i],
+								log_Lphot[i])
+					else:
+						magnitudes_spots = np.column_stack((magnitudes_spots,
+								isochrone.colorize(Tspot[i],nlog_g[i],
+								log_Lspot[i]) +	isochrone.colorize(Tphot[i],
+								nlog_g[i],log_Lphot[i])))
 
-#Compute magnitudes of the photosphere of spotted stars
-mmagnitudes_spots=[]
-for i in np.arange(len(Tspot)):
-	if i == 0:
-		magnitudes_spots =isochrone.colorize(Tspot[i],nlog_g[i],
-				log_Lspot[i]) + \
-				isochrone.colorize(Tphot[i],nlog_g[i],log_Lphot[i])
-	else:
-		magnitudes_spots = np.column_stack((magnitudes,
-				isochrone.colorize(Tspot[i],nlog_g[i],log_Lspot[i]) +
-				isochrone.colorize(Tphot[i],nlog_g[i],log_Lphot[i])))
-
-#Save spotted isochrone
-isochrone.save_spotted(spots_params,isodata_spots,magnitudes_spots)
+				#Save spotted isochrone
+				isochrone.save_spotted(spots_params,isodata_spots,
+						magnitudes_spots)
