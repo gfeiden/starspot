f696d0877cf7fea7cd1e28d53b52dcc6b872df3e
gfeiden <gregory.a.feiden@gmail.com>
2015-07-07 12:01:54 +0200

    updated init file

--- starspot/__init__.py
+++ starspot/__init__.py
@@ -3,8 +3,8 @@
 __all__ = ["model", "color"]
 __version__ = "0.0.1"
 
-__author__ = "Gregory A. Feiden (gregory.feiden@physics.uu.se)"
+__author__ = "Gregory A. Feiden <gregory.feiden@physics.uu.se>"
 __copyright__ = "Copyright 2015 Gregory A. Feiden"
-_contributors = [""]
+_contributors = ["Steven Christophe <steven.christophe@u-psud.fr>"]
 
 from model import *
